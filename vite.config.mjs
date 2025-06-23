c0015832c73aeb0a725d7df2054d1305c14adca9
mohamed <2000gaber2000@gmail.com>
2025-06-23 17:02:50 +0300

    vitr

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 export default defineConfig(() => {
   return {
-    base: '/Eshopping_Dashboard/',
+    base: '/',
     build: {
       outDir: 'dist',
     },
