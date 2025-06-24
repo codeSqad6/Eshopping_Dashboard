eae8d0a06fc5ee17e7d7848d0ca13f0fdb2ad017
ZiadSafwat <104617951+ZiadSafwat@users.noreply.github.com>
2025-06-24 20:33:41 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/Eshopping_Dashboard/',
     build: {
       outDir: 'dist',
     },
